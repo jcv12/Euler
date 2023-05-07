adcf37581f17c1ffc1c4ae2e40dd65f2fb07f498
jcv12 <john.c.vayianos@gmail.com>
2023-05-06 21:23:47 -0400

    Problem 1 Completed

---
+++ Problem01.py
@@ -0,0 +1,15 @@
+i = 1
+list = []
+
+# Create loop to go through natural numbers below 1000
+while i < 1000:
+    if i % 3 == 0: # Make sure it is multiple of 3
+        print(i)
+        list.append(i) # Store in list then get the sum
+    elif i % 5 == 0:
+        print(i)
+        list.append(i) # Make sure it is multiple of 5
+    i += 1
+
+answer = sum(list) # Grab the total
+print(answer)
